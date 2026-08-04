0900f269a424c4b5ca4ee529c189db7584317e3f
shanmukh7227-ship-it <shanmukh7227@gmail.com>
2026-08-04 10:48:46 +0000

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
-from flask import Flask
+from flask import Flask,render_template
+
 app=Flask(__name__)
 @app.route('/')
 def home():
-    return "hello world!"
-if __name__=='__main__':
+    return render_template("index.html")
+@app.route("/about")
+def about():
+    return render_template("about.html")
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+@app.route("/courses")
+def courses():
+    return render_template("courses.html")
+@app.route("/trainers")
+def trainers():
+    return render_template("trainers.html")
+
+@app.route('/register',method="POST","GET")
+def register():
+    if request.method=="POST":
+        name=request.form["name"]
+        email=request.form["email"]
+        password=request.form["password"]
+        dob=request.form["dob"]
+        gender=request.form["gender"]
+        course=request.form["course"] 
+        return render_template("register.html")       
+
+if __name__ == '__main__':
     app.run(debug=True)
